83c9da9e0d0a2414885dc2081ba2a46b47b1092a
Apurv Verma <dapurv5@gmail.com>
2015-11-07 08:17:27 -0500

    begin java code

--- src/main/java/com/gatech/graphcompression/GraphCompression.java
+++ src/main/java/com/gatech/graphcompression/GraphCompression.java
@@ -1,7 +1,21 @@
 package com.gatech.graphcompression;
 
 public class GraphCompression {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+  
+  
+  public static void main( String[] args ) {
+    //String path = "/home/dapurv5/Desktop/Semesters/1st_semester/GRA/cnr-2000.graph";
+    //String path = "/home/dapurv5/Desktop/Semesters/1st_semester/GRA/audikw1.graph";
+    //String path = "/home/dapurv5/Desktop/Semesters/1st_semester/GRA/citationCiteseer.graph";
+    //String path = "/home/dapurv5/Desktop/Semesters/1st_semester/GRA/preferentialAttachment.graph";
+    String path = "/home/dapurv5/Desktop/Semesters/1st_semester/GRA/smallworld.graph";
+    cct cct = new cct();
+    cct.readGraphDIMACS(path);
+    int[] off = cct.off;
+    int[] ind = cct.ind;
+    
+    for(int i = 0; i < 10; i++) {
+      System.out.print(off[i]+",");
     }
+  }
 }
